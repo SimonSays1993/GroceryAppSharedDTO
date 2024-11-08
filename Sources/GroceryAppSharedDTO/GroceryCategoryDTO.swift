7deded0e4faf55666982db698ee9ce06e79dac03
Simon McNeil <simonmcneil062@gmail.com>
2024-11-08 08:31:43 -0500

    had to make inits public

--- Sources/GroceryAppSharedDTO/GroceryCategoryDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryDTO.swift
@@ -5,7 +5,7 @@ public struct GroceryCategoryResponseDTO: Codable {
     public let title: String
     public let colorCode: String
     
-    init(id: UUID, title: String, colorCode: String) {
+    public init(id: UUID, title: String, colorCode: String) {
         self.id = id
         self.title = title
         self.colorCode = colorCode
@@ -16,7 +16,7 @@ public struct GroceryCategoryRequestDTO: Codable {
     public let title: String
     public let colorCode: String
     
-    init(title: String, colorCode: String) {
+    public init(title: String, colorCode: String) {
         self.title = title
         self.colorCode = colorCode
     }
