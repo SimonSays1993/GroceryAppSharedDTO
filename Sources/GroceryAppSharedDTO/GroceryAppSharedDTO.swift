8bf0a010669fdb50d42e8fc6da6fc5ea777fcd59
Simon McNeil <simonmcneil062@gmail.com>
2024-11-07 21:52:03 -0500

    Initial Commit

---
+++ Sources/GroceryAppSharedDTO/GroceryAppSharedDTO.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct GroceryAppSharedDTO {
+    public private(set) var text = "Hello, World!"
+    
+    public init() {
+        
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Simon Mcneil on 2024-11-07.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String?
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
